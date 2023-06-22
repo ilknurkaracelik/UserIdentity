523cd4d6c7033901db0f6ac157e13e75cac982d6
ilknur karaçelik <ilknurkaracelik@hotmail.com>
2023-06-22 09:16:41 +0300

    Add project files.

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Identity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public AccountController()
+        {
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new IdentityDataContext()));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+
+                var result = userManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+
+
+            return View(model);
+        }
+    }
+}
